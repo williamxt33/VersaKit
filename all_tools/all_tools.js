e638a79dfa2e2b1a878d90f0966f59c069684154
william <williamyen12292010@gmail.com>
2025-10-25 17:52:35 +0800

    fix ham bar

--- all_tools/all_tools.js
+++ all_tools/all_tools.js
@@ -1,8 +1,17 @@
 function toggleMenu() {
     const sideMenu = document.getElementById('side-menu');
-    const hamMenu = document.querySelector('.ham-menu');
+    const hamMenu = document.getElementById('ham-menu');
     
     sideMenu.classList.toggle('show');
     hamMenu.classList.toggle('active');
 }
 
+document.addEventListener('click', function(event) {
+    const sideMenu = document.getElementById('side-menu');
+    const hamMenu = document.getElementById('ham-menu');
+    
+    if (!sideMenu.contains(event.target) && !hamMenu.contains(event.target)) {
+        sideMenu.classList.remove('show');
+        hamMenu.classList.remove('active');
+    }
+});
